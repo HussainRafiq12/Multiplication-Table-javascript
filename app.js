74e4e462e3e8866b605bf556dd067e63bf558526
Hussain Rafiq <160489258+HussainRafiq12@users.noreply.github.com>
2024-09-21 17:12:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let inputValue = prompt("enter your Table");
+for (let i = inputValue; i >= 10; i--){
+    for (let j = 1; j <= 10; j++)
+     {
+   document.write(i + "x" + j +  "=" +i * j );
+   document.write( "<br>")
+  
+    }
+    document.write( "<br>")
+} 
+
